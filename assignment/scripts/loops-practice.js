f4016c68ce7224fb58c145888fad4dda6117d105
Dylan Roets <dylanroets@yahoo.com>
2022-10-09 22:47:28 -0500

    crashed chrome a few more times but got it git status

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -33,7 +33,7 @@ for (let i=2; i<11; i+=2) {
 }
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-for (let i=5; i<6; i++) {
+for (let i=5; i>=0; i--) {
   console.log(i);
 }
 
@@ -43,7 +43,9 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for ( starNames of stars) {
+  console.log(starNames);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');

70a58d68ab55e9bbef02d5c08826741047d3bbd6
Dylan Roets <dylanroets@yahoo.com>
2022-10-09 22:27:33 -0500

    going from some stretch goals and crashed the html a couple times lol

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -13,20 +13,29 @@ for (let i=0; i<4; i++) {
 // 1.a. TODO: Write a for loop to console.log the numbers from 0 to 5 
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 0 to 5');
+// you need to change the i length to i<6
 for (let i=0; i<6; i++) {
   console.log(i);
 }
 // 1.b. TODO: Write a for loop to console.log the numbers from 3 to 5
 //   - Which part of the example loop do you need to change to do this?
 console.log('count from 3 to 5');
-
+// you need to change the starting i to 3
+for (let i=3; i<6; i++) {
+  console.log(i);
+}
 // 1.c. TODO: Write a for loop to console.log EVEN numbers from 2 to 10
 //   - Which part of the example loop do you need to change to do this?
 console.log('count even numbers from 2 to 10 (2, 4, 6, 8, 10):');
-
+// i changed the increment to change by 2 instaed of 1, also had to change the index length to ccount for counting to a value of 10 so i<11
+for (let i=2; i<11; i+=2) {
+  console.log(i);
+}
 // 1.d. (STRETCH) TODO: Write a for loop to do a countdown from 5 to 0
 console.log('STRETCH: countdown from 5 to 0');
-
+for (let i=5; i<6; i++) {
+  console.log(i);
+}
 
 // 2. For of loops  
 console.log('---- 2. For Of loop ----');
